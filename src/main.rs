4c22e2f68f55e21d91a70b2788cb49cefad33ce9
Stefano Varesi <stefano.varesi@gmail.com>
2022-03-26 20:11:05 +0100

    feat: rover has a default initial position

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct MarsRover {
+    x: u8,
+    y: u8,
+}
+
+impl Default for MarsRover {
+    fn default() -> Self {
+        MarsRover {
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::MarsRover;
+
+    #[test]
+    fn default_initial_position() {
+        let mars_rover = MarsRover::default();
+
+        assert_eq!(mars_rover.x, 0);
+        assert_eq!(mars_rover.y, 0);
+    }
+}
